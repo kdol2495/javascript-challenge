889f0740997d8539f7daf4bae3774258438244ea
kdol2495 <53966704+kdol2495@users.noreply.github.com>
2019-12-15 23:24:06 -0800

    append table

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,16 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach((data) => {
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
