0a02a3ecab863341c1cd05a00f1867ca092b4955
kdol2495 <53966704+kdol2495@users.noreply.github.com>
2019-12-16 00:24:59 -0800

    finished js files, css and images

--- static/js/app.js
+++ static/js/app.js
@@ -3,15 +3,38 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
+// select table body
 var tbody = d3.select("tbody");
 
-console.log(data);
-
-data.forEach((data) => {
+// append rows and cells
+function table(data) {
+data.forEach(function(visions) {
     var row = tbody.append("tr");
-    Object.entries(data).forEach(([key, value]) => {
+    Object.entries(visions).forEach(([key, value]) => {
         var cell = row.append("td");
         cell.text(value);
     });
-});
 
+});
+};
+table(tableData);
+
+// Select button from html
+var button = d3.select("button");
+
+// Add event listener
+button.on("click", function() {
+
+    d3.event.preventDefault();
+    // select input element
+    var inputElement = d3.select(".form-control");
+    // grab value
+    var inputValue = inputElement.property("value");
+    // data filter
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+    // clear rows
+    tbody.selectAll("tr").remove();
+
+    table(filteredData);
+
+ });
